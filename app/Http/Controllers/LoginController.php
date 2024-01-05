732dd175301ed3a75756d3128f0aee9374262754
IgorOliveira852 <igor.clinton.007@gmail.com>
2024-01-05 19:41:02 -0300

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\LoginNeedsVerification;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function submit(Request $request)
+    {
+        // validar o numero de telefone
+        $request->validate([
+           'phone' => 'required|numeric|min:9'
+        ]);
+
+        // encontrar ou criar um user model
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!$user){
+            return response()->json(['message' => 'Não foi possível encontrar o usuario com o respectivo numero.'], 401);
+        }
+
+        // enviar um codigo ao usuario
+        $user->notify(new LoginNeedsVerification());
+
+        // retornar um response
+        return response()->json(['message' => 'menssagem de texto enviada.']);
+    }
+
+    public function verify(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required|numeric|min:9',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+
+        if ($user) {
+            $user->update([
+                'login_code' => null
+            ]);
+
+           return $user->createToken($request->login_code)->plainTextToken;
+        }
+
+        return response()->json(['message' => 'codigo de verificação invalido.'], 401);
+    }
+}
